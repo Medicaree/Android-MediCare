e80a5558544ebb47fc17cc295e6b03647d7c2a7d
Medicare <caree.medi@gmail.com>
2022-03-29 14:24:08 +0500

    2nd

--- app/src/main/java/com/android/medicareapp/Utils/Constants.java
+++ app/src/main/java/com/android/medicareapp/Utils/Constants.java
@@ -1,6 +1,7 @@
 package com.android.medicareapp.Utils;
 
 public class Constants {
+    public static String _as = "AS";
     public static String as = "AS";
     public static String customer = "C USTOMER";
     public static String business = "BUSINESS";
